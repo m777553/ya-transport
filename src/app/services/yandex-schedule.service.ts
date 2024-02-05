055cf0249b1ade9194b91bc8a84462d6bbed2098
Marina <m777553@ya.ru>
2024-02-05 14:41:44 +0300

    Task-2 feat: add proxy to fix cors

--- src/app/services/yandex-schedule.service.ts
+++ src/app/services/yandex-schedule.service.ts
@@ -5,11 +5,11 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class YandexScheduleService {
-  private baseUrl = 'https://api.rasp.yandex.net/v3.0/';
+  private baseUrl = '/v3.0/';
 
   constructor(private _http: HttpClient) { }
 
   getSchedule(transportType: string, from: string, to: string, date: string) {
-    return this._http.get(`${this.baseUrl}search/?apikey=<api-key>&format=json&from=${from}&to=${to}&lang=ru_RU&transport_types=${transportType}&date=${date}`);
+    return this._http.get(`${this.baseUrl}search/?apikey=<ea9ee51f-aa56-4e1a-909f-61fbce3cdd02>&format=json&from=${from}&to=${to}&lang=ru_RU&transport_types=${transportType}&date=${date}`);
   }
 }
